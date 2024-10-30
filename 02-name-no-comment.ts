07c17abab66876d5cbe06ec54c8db33e7836774e
Carolina <179249348+DevCarolinaCh@users.noreply.github.com>
2024-10-30 01:28:21 -0300

    Add files via upload

---
+++ 02-name-no-comment.ts
@@ -0,0 +1,53 @@
+(() => {
+
+  
+    const tempCelsius = [33.6, 12.34];
+
+    
+    const portIp = '123.123.123.123';
+
+    const clients = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    const emails = clients.map( clients => clients.email );
+
+    const couldJump = false;
+    const vouldRun = true;
+    const hasItems = true;
+    const gettingLoading = false;
+
+    
+    const startInTime = new Date().getTime();
+  
+    const endInTime = new Date().getTime() - startInTime;
+
+
+
+    function geetingBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+   
+    function gettingBooksUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+ 
+    function getSquareArea( side: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
+
+
+
+
+
---
+++ 02-name-types.ts
@@ -0,0 +1,62 @@
+(() => {
+
+    // arreglo de temperaturas celsius
+    const tempCelsius = [33.6, 12.34];
+
+    // Dirección ip del servidor
+    const portIp = '123.123.123.123';
+
+    // Listado de usuarios
+    const clients = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    // Listado de emails de los usuarios
+    const emails = clients.map( clients => clients.email );
+
+    // Variables booleanas de un video juego
+    const couldJump = false;
+    const vouldRun = true;
+    const hasItems = true;
+    const gettingLoading = false;
+
+    // Otros ejercicios
+    // tiempo inicial
+    const startInTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+    // Tiempo al final
+    const endInTime = new Date().getTime() - startInTime;
+
+
+    // Funciones
+    // Obtiene los libros
+    function geetingBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    // obtiene libros desde un URL
+    function gettingBooksUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+    // obtiene el área de un cuadrado basado en sus lados
+    function getSquareArea( side: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    // imprime el trabajo
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
+
+
+
+
+
